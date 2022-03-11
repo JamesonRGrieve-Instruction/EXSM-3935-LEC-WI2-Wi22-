c720c0851ac90fb34f1eb46b6e75c9ef9954b649
James Grieve <jgrieve@ualberta.ca>
2022-03-10 20:58:54 -0700

    Week 1 notes;

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,57 @@
 async function main() {
     // This is where the code you're actually experimenting with goes.
-    print("Hello, world!");
     
-    let name = await input("Please enter your name: ");
+    // print("Hello, world!");
+    // let name = await input("Please enter your name: ");
 
-    while (name != "bob") 
-    {
-        print("Hello, "+name+"!");
-        name = await input("Please enter your name: ");
-    }
+    // Boolean
+    let myBoolean = true; // True or false, yes or no, on or off.
+
+    // Number 
+    let myNumber = 3.14; // Numbers in JavaScript can be either integers or decimal values.
+
+    // String
+    let myString = "Hello"; // Strings are collections of characters delimited by quotation marks.
+    
+    /* Arithmetic Operators
+    Concatenation Operator: + (Combine two string values)
+    Addition Operator: + (Add two number values)
+    [Which operation takes place depends on the data type of the operands]
+    Subtraction Operator: - (Subtract the second number from the first number)
+    Multiplication Operator: * (Multiply two number values)
+    Division Operator: / (Divide the first number by the second number)
+    Modulus Operator: % (Get the remainder of a given integer division)
+    */
+
+    /* Casing 
+    this-is-an-example-of-kebab-case
+
+    thisIsAnExampleOfCamelCase (usually the default for JS variables)
+
+    ThisIsAnExampleOfPascalCase (usually the default for JS classes)
+
+    this_is_an_example_of_snake_case
+
+    THIS_IS_AN_EXAMPLE_OF_SCREAMING_SNAKE_CASE
+    */
+
+    /* Relational Operators
+    > Greater Than?
+    < Less Than?
+    >= Greater Than, Or Equal To?
+    <= Less Than, or Equal To?
+    != Not Equal To
+    == Equal To
+    === Identity
+    */
+
+    /* Logical Operators
+    && And (Returns true if both operands are true, false otherwise)
+    || Or (Returns true if either operand is true, false if both are false)
+    ! Not (Reverses a boolean value)
+    ^ Xor (Returns true if exactly one operand is true)
+    */
+
+    print(5 === "5");
 }
 main();
