430d910b29aaca2af502e62bf0ce2c7973f44a22
James Grieve <jgrieve@ualberta.ca>
2022-04-07 19:26:09 -0600

    Single-time validation;

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 async function main() {
-    
+    let userNumber = Number(await input("Please enter an integer between 1 and 10: "));
+    if (userNumber >= 1 && userNumber <= 10 && Number.isInteger(userNumber))
+    {
+        output("Validation passed!");
+    }
+    else
+    {
+        output("Validation failed.");
+    }
 }
 
