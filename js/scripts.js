c0f193db00085692e54516a1b4e0a3fa20379216
James Grieve <jgrieve@ualberta.ca>
2022-04-07 19:08:15 -0600

    Super basic function;

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-async function main() {
-
+function greeting() {
+    output("Hello.");
+    output("How are you?");
+    output("How's the weather?");
 }
+async function main() {
+    greeting();
+    greeting();
+    greeting();
+    greeting();
+    greeting();
+}
+
