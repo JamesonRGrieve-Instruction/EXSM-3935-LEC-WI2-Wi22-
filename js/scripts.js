abfae1dfb2f9d0db452ab69b06d6aa1eeabd8437
James Grieve <jgrieve@ualberta.ca>
2022-04-07 19:18:19 -0600

    Function examples;

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
+async function main() {
+    greeting();
+
+    greet("James");
+    greet("John");
+    greet("Jane");
+
+    output(giveMePi());
+    output(addThese(42, 12));
+}
+
+// Function with no parameters and no returns.
 function greeting() {
     output("Hello.");
     output("How are you?");
     output("How's the weather?");
 }
-async function main() {
-    greeting();
-    greeting();
-    greeting();
-    greeting();
-    greeting();
+
+// Function with a single parameter and no returns.
+function greet(person) {
+    output(`Hello, ${person}! How are you?`);
 }
 
+// Function with no parameters and a return.
+function giveMePi() {
+    return 3.141592654;
+}
+
+// Function with two parameters and a return.
+function addThese(number1, number2)
+{
+    return number1 + number2;
+}
