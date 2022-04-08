06afc945ec87af4cb7b81932eee539c225663f01
James Grieve <jgrieve@ualberta.ca>
2022-04-07 19:22:23 -0600

    Code reset;

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,4 @@
 async function main() {
-    greeting();
-
-    greet("James");
-    greet("John");
-    greet("Jane");
-
-    output(giveMePi());
-    output(addThese(42, 12));
+    
 }
 
-// Function with no parameters and no returns.
-function greeting() {
-    output("Hello.");
-    output("How are you?");
-    output("How's the weather?");
-}
-
-// Function with a single parameter and no returns.
-function greet(person) {
-    output(`Hello, ${person}! How are you?`);
-}
-
-// Function with no parameters and a return.
-function giveMePi() {
-    return 3.141592654;
-}
-
-// Function with two parameters and a return.
-function addThese(number1, number2)
-{
-    return number1 + number2;
-}
